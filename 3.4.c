ba74ad33b3fbafd40bf7d2075ef9d932e9821146
refaso <zajcevmaksim556@gmail.com>
2024-12-15 18:18:26 +0300

    Update 3.4.c

--- 3.4.c
+++ 3.4.c
@@ -2,63 +2,99 @@
 #include <locale.h>
 
 #define MAX_IDENTIFIERS 36
-#define MAX_LENGTH 12+1  
+#define MAX_LENGTH 12 + 1
 
-static void extractID(char identifiers[MAX_IDENTIFIERS][MAX_LENGTH], int* count) {
-    char c;
-    int j = 0;
+int isValidChar(char c, int position) {
+    return (c >= 'A' && c <= 'Z') || (c == '_') || (c >= '0' && c <= '9');
+}
 
-    while ((c = getchar()) != '.' && *count < MAX_IDENTIFIERS) {
-        if (c == '\n') {
-            continue;
+int isValidIdentifier(char identifier[]) {
+    if (identifier[0] == '\0') {
+        return 0; 
+    }
+
+    if (identifier[0] >= '0' && identifier[0] <= '9') {
+        return 0; 
+    }
+
+    for (int i = 0; identifier[i] != '\0'; i++) {
+        if (!isValidChar(identifier[i], i)) {
+            return 0; 
         }
+    }
+
+    return 1; 
+}
+
+int extractIdentifiers(char identifiers[MAX_IDENTIFIERS][MAX_LENGTH]) {
+    int count = 0;
+    int length = 0;
+    char c;
+
+    while ((c = getchar()) != '.' && count < MAX_IDENTIFIERS) {
+        if (c == '\n') {
+            continue; 
+        }
+
         if (c == ' ') {
-            if (j > 0) {
-                identifiers[*count][j] = '\0';
-                (*count)++;
-                j = 0;
+            if (length > 0) {
+                identifiers[count][length] = '\0'; 
+
+                if (isValidIdentifier(identifiers[count])) {
+                    count++;
+                }
+
+                length = 0; 
             }
         }
         else {
-            if (j < MAX_LENGTH - 1) {
-                identifiers[*count][j++] = c;
+            if (length < MAX_LENGTH - 1) {
+                identifiers[count][length++] = c;
             }
         }
     }
-    if (j > 0) {
-        identifiers[*count][j] = '\0';
-        (*count)++;
+
+
+    if (length > 0) {
+        identifiers[count][length] = '\0'; 
+
+        if (isValidIdentifier(identifiers[count])) {
+            count++;
+        }
     }
+
+    return count;
 }
 
-static void printUpperID(char identifiers[MAX_IDENTIFIERS][MAX_LENGTH], int count) {
+int isAllUpper(char identifier[]) {
+    for (int i = 0; identifier[i] != '\0'; i++) {
+        if (identifier[i] >= 'a' && identifier[i] <= 'z') {
+            return 0; 
+        }
+    }
+    return 1; 
+}
+
+void printUpperIDs(char identifiers[MAX_IDENTIFIERS][MAX_LENGTH], int count) {
     int hasUpperID = 0;
     for (int i = 0; i < count; i++) {
-        int isUpper = 1;
-        for (int k = 0; identifiers[i][k] != '\0'; k++) {
-            if (identifiers[i][k] < 'A' || identifiers[i][k] > 'Z') {
-                isUpper = 0;
-                break;
-            }
-        }
-        if (isUpper) {
+        if (isAllUpper(identifiers[i])) {
             printf("%s\n", identifiers[i]);
-            hasUpperID = 1;
+            hasUpperID = 1; 
         }
     }
     if (!hasUpperID) {
-        printf("нет слов в верхнем регистре\n"); 
+        printf("Нет идентификаторов с заглавными буквами\n"); 
     }
 }
 
 int main() {
-    int count = 0;
     char identifiers[MAX_IDENTIFIERS][MAX_LENGTH];
     setlocale(0, "");
 
     printf("Введите последовательность идентификаторов, оканчивающуюся точкой:\n");
-    extractID(identifiers, &count);
-    printUpperID(identifiers, count);
+    int count = extractIdentifiers(identifiers);
+    printUpperIDs(identifiers, count); 
 
     return 0;
 }
