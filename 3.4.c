4dcfec8e51ddad51d842a0c0512ce3bbf3ea902e
refaso <zajcevmaksim556@gmail.com>
2024-12-18 11:25:53 +0300

    Update 3.4.c

--- 3.4.c
+++ 3.4.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <locale.h>
 
-#define MAX_IDENTIFIERS 36
-#define MAX_LENGTH 12 + 1
+#define MAX_IDENTIFIERS 4
+#define MAX_LENGTH 4
 
-int isValidChar(char c, int position) {
+int isValidChar(char c) {
     return (c >= 'A' && c <= 'Z') || (c == '_') || (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z');
 }
 
@@ -18,23 +18,22 @@ int isValidIdentifier(char identifier[]) {
     }
 
     for (int i = 0; identifier[i] != '\0'; i++) {
-        if (!isValidChar(identifier[i], i)) {
+        if (!isValidChar(identifier[i])) {
             return 0;
         }
     }
-
     return 1;
 }
 
-int findIDs(char identifiers[MAX_IDENTIFIERS][MAX_LENGTH]) {
+int findIDs(char identifiers[MAX_IDENTIFIERS][MAX_LENGTH], int* exceedsLength) {
     int count = 0;
     int length = 0;
     char c;
-    int better_lenght = 0;
-    int maxCount = 0;
+
+    *exceedsLength = 0;
 
     while ((c = getchar()) != '.' && count < MAX_IDENTIFIERS) {
-        if (c == '\n') {
+        if (c == '\n' || c == '\r') {
             continue;
         }
 
@@ -42,12 +41,15 @@ int findIDs(char identifiers[MAX_IDENTIFIERS][MAX_LENGTH]) {
             if (length > 0) {
                 identifiers[count][length] = '\0';
 
+                if (length >= MAX_LENGTH - 1 && *exceedsLength == 0) {
+                    printf("Идентификатор(ы) превышает максимальную длину %d символов\n", MAX_LENGTH - 1);
+                    *exceedsLength = 1;
+                }
+
                 if (isValidIdentifier(identifiers[count])) {
                     count++;
-                    if (count == MAX_IDENTIFIERS) {
-                        maxCount = 1;
-                    }
                 }
+
                 length = 0;
             }
         }
@@ -55,29 +57,31 @@ int findIDs(char identifiers[MAX_IDENTIFIERS][MAX_LENGTH]) {
             if (length < MAX_LENGTH - 1) {
                 identifiers[count][length++] = c;
             }
-            else if (!better_lenght) {
-                printf("Идентификатор превышает максимальную длину %d символов\n", MAX_LENGTH - 1);
-                better_lenght = 1;
+            else if (*exceedsLength == 0) {
+                printf("Идентификатор(ы) превышает максимальную длину %d символов\n", MAX_LENGTH - 1);
+                *exceedsLength = 1;
             }
         }
+    }
+
+    if (c != '.' && count >= MAX_IDENTIFIERS) {
+        printf("Превышено максимальное количество идентификаторов: %d\n", MAX_IDENTIFIERS);
+        return -1;
     }
 
     if (length > 0) {
         identifiers[count][length] = '\0';
+        if (length >= MAX_LENGTH - 1 && *exceedsLength == 0) {
+            printf("Идентификатор(ы) превышает максимальную длину %d символов\n", MAX_LENGTH - 1);
+            *exceedsLength = 1;
+        }
+
         if (isValidIdentifier(identifiers[count])) {
             count++;
-            if (count == MAX_IDENTIFIERS) {
-                maxCount = 1;
-            }
         }
     }
 
-    if (maxCount) {
-        return -1; 
-    }
-    else {
-        return count; 
-    }
+    return count;
 }
 
 int isAllUpper(char identifier[]) {
@@ -105,6 +109,7 @@ void printUpperIDs(char identifiers[MAX_IDENTIFIERS][MAX_LENGTH], int count) {
             hasUpperID = 1;
         }
     }
+
     if (!hasUpperID) {
         printf("Нет идентификаторов с заглавными буквами\n");
     }
@@ -115,7 +120,13 @@ int main() {
     setlocale(0, "");
 
     printf("Введите последовательность идентификаторов, оканчивающуюся точкой:\n");
-    int count = findIDs(identifiers);
+    int exceedsLength;
+    int count = findIDs(identifiers, &exceedsLength);
+
+    if (exceedsLength || count < 0) {
+        return 0;
+    }
+
     printUpperIDs(identifiers, count);
 
     return 0;
